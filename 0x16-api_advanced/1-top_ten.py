526b61d472c29517d73583fba170407e21509c61
niyikomeyeeric <niyikomeye@gmail.com>
2022-11-01 17:22:45 +0200

    fixing  errors

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-""" Reddit API. """
-
-import requests
+"""Module for task 1"""
 
 
 def top_ten(subreddit):
-    """ Function query in API of reddit.com and return
-    the number of subscribers.
-    """
-    respn = requests.get(
-        'https://www.reddit.com/r/{}/hot.json?limit=10'.format(
-            subreddit), allow_redirects=False)
-    if respn.status_code == 200:
-        respn_data_json = respn.json()
-        for key_title in respn_data_json['data']['children']:
-            if 'data' in key_title:
-                print(key_title['data']['title'])
+    """Queries the Reddit API and returns the top 10 hot posts
+    of the subreddit"""
+    import requests
+
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        print('None')
     else:
-        print(None)
+        [print(child.get("data").get("title"))
+         for child in sub_info.json().get("data").get("children")]
